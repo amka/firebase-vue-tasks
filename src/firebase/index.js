8b6c72e23e8cd5430c2f773cb430ec91abf54139
Andrey M <meamka@ya.ru>
2019-03-14 16:26:50 +0300

    Remove deprecated firebase settings

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -18,11 +18,6 @@ firebase.initializeApp(config)
 // Initialize Cloud Firestore through Firebase
 var db = firebase.firestore()
 
-// Disable deprecated features
-db.settings({
-  timestampsInSnapshots: true
-})
-
 export {
   db
 }
